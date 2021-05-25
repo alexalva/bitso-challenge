7bf8badfc5e126ee1389c2d142ab4f1cb27a4782
alexalva <57193170+alexalva@users.noreply.github.com>
2021-05-24 19:09:11 -0500

    Update Lunes

--- desafio.py
+++ desafio.py
@@ -20,21 +20,37 @@ import statsmodels.api as sm
 from statsmodels.graphics.tsaplots import plot_acf
 from statsmodels.stats.diagnostic import het_arch
 
+from arch.unitroot import engle_granger
+
 
 
 df = yf.download(tickers='BTC-USD', start="2015-01-01", end="2020-12-31", interval = '1d')
 # df['log return'] = np.log(df['Close']).diff()
 
-df['log return'] = np.log(df['Close']/df['Close'].shift(1))
-
 df['log Close'] = np.log(df['Close'])
+df['log return'] = np.log(df['Close']/df['Close'].shift(1)).fillna(0)
 
-serie = np.correlate(df['log Close'], df['log Close'], mode='full')
+logrtn = np.log(df['Close']/df['Close'].shift(1)).fillna(0)
+logrtn.plot()
 
-serie = pd.Series(sm.tsa.acf(df['log Close'], nlags=5))
+# serie = np.correlate(df['log Close'], df['log Close'], mode='full')
+# serie = pd.Series(sm.tsa.acf(df['log Close'], nlags=5))
 
 
-df['log Close'] = np.log(df['Close'])
+# plot_acf(df['log Close'])
 
-plot_acf(df['log Close'])
-plot_acf(df['log return'])
+#Jungle-Box Text for Serial Corelation
+sm.stats.acorr_ljungbox(logrtn, lags=[5], return_df=True)
+
+#Null
+
+plot_acf(logrtn)
+
+eg_test = engle_granger(logrtn, logrtn, trend="n")
+print(eg_test)
+
+
+res = sm.tsa.ARMA(logrtn, (1,1)).fit(disp=-1)
+print(sm.stats.diagnostic.het_arch(res.resid, nlags=12))
+
+plot_acf(logrtn)
